8e03a1b3d4ba3a513fe510b6f89c14d96aa70a8c
Andreea Hanson <andreea.hanson7@gmail.com>
2020-02-17 23:05:44 -0700

    Go through strict options in tsconfig.json

--- src/analytics/analytics.ts
+++ src/analytics/analytics.ts
@@ -1,6 +1,16 @@
-console.log('Sending...')
+// console.log('Sending...')
 
-// run in the command line: tsc -init
-// so that it runs the changes in any file, not a specified one
-// you need to navigate into the right folder!!!!
-// after that, you can run only tsc or tsc -w in the command line, and it will look at all files automatically
+// // run in the command line: tsc -init
+// // so that it runs the changes in any file, not a specified one
+// // you need to navigate into the right folder!!!!
+// // after that, you can run only tsc or tsc -w in the command line, and it will look at all files automatically
+
+let logged;
+
+function sendAnalytics(data: string)  {
+  console.log(data);
+  logged = true;
+  console.log(logged);
+}
+
+sendAnalytics('The data');
--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,9 @@
 
 const button = document.querySelector('button');
 
-button?.addEventListener('click', () => {
-  console.log('clicked')
-});
+function clickHandler(message: string) {
+  console.log('Clicked' + message);
+}
+if(button) {
+  button?.addEventListener('click', clickHandler.bind(null, 'You are welcome'));
+}
