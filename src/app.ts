ea7ce360217917f8adc579e8c81c2bad1227be46
Andreea Hanson <andreea.hanson7@gmail.com>
2020-02-17 22:51:47 -0700

    Set compiler options in tsconfig.json

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+// // compiler settings, so that we don't need the tsc app.ts command to refresh the page
+
+// const userName = 'Maximilian';
+
+// console.log(userName)
+
+// // in the command line: tsc app.ts --watch or tsc app.ts -w
+// // => Watch mode
+
+// //downside: specifically have to target the app.ts file
+
+
+const button = document.querySelector('button');
+
+button?.addEventListener('click', () => {
+  console.log('clicked')
+});
